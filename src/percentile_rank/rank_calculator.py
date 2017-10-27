2eb5b4da06093c4795eeb040ec88801afe160396
Michael Tintiuc <overcrookd@gmail.com>
2017-10-28 00:49:06 +0300

    Added stub classes. Tested file read/write

---
+++ src/percentile_rank/rank_calculator.py
@@ -0,0 +1,10 @@
+class RankCalculator():
+    """
+    Calculate percentile rank based on index
+    """
+
+    def __init__(self):
+        self.calculate()
+
+    def calculate(self):
+        pass
